789bf6a5920c52b084b966305bc9d910689b306c
eperez9 <300911292@cencol.ca>
2019-09-06 15:22:14 -0400

    DVD screensaver

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,58 +1,71 @@
 //Immediate Invoked Anonymous Function
-(function () {
-    var canvas = document.getElementById("canvas");
-    var stage;
-    var helloLabel;
-    var upDirection;
-    var rightDirection;
+
+(function(){
+
+    let canvas = document.getElementById("canvas");
+    let stage : createjs.Stage;
+    let helloLabel : createjs.Text;
+    
+    let upDirection : boolean;
+    let rightDirection: boolean;
+
     function Init() {
         console.log("Initialization Start");
         Start();
     }
+
     function Start() {
         console.log("Starting application...");
+
         //initialize createjs
         stage = new createjs.Stage(canvas);
         createjs.Ticker.framerate = 60; //60fps
         createjs.Ticker.on("tick", Update);
+
         upDirection = true;
         rightDirection = true;
         Main();
+
     }
+
     function Update() {
         stage.update();
-        if (rightDirection) {
+        if (rightDirection){
             helloLabel.x += 1;
         }
-        else {
+        else{
             helloLabel.x -= 1;
+
         }
-        if (upDirection) {
+        if (upDirection){
             helloLabel.y += 1;
+
         }
-        else {
+        else{
             helloLabel.y -= 1;
         }
-        if (helloLabel.x > 600 && rightDirection) {
+
+        if (helloLabel.x > 600 && rightDirection){
             rightDirection = false;
         }
-        else if (helloLabel.x < 0 && !rightDirection) {
+        else if (helloLabel.x < 0 && !rightDirection){
             rightDirection = true;
         }
-        if (helloLabel.y > 440 && upDirection) {
+        if (helloLabel.y > 440 && upDirection){
             upDirection = false;
         }
-        else if (helloLabel.y < 0 && !upDirection) {
+        else if (helloLabel.y < 0 && !upDirection){
             upDirection = true;
         }
     }
-    function Main() {
+
+    function Main() {        
         console.log("Game Start!");
         helloLabel = new createjs.Text("Hello World!", "40px Consolas", "#000000");
         helloLabel.x = 100;
         helloLabel.y = 100;
         stage.addChild(helloLabel);
+
     }
     window.onload = Init;
 })();
-//# sourceMappingURL=game.js.map
